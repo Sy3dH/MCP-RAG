56e70724663aa88bbfe61f2fd74bd27fd966f10a
Sy3dH <hamza.tahir@imaginationai.net>
2025-06-26 12:50:29 +0500

    Replaced system prompt with Upload dir

--- configs/constants.py
+++ configs/constants.py
@@ -1,9 +1,4 @@
 vector_store_path= r"D:\9D Tech Work\Central_repo\Demo\vector_store"
 MCP_SERVER_URL = "http://localhost:8001/mcp"
 SIMILARITY_THRESHOLD = 0.95
-SYSTEM_PROMPT = ("You are a research assistant, that will help answer the queries of the users. When gotten a query, here"
-                 "are the things you will do:"
-                 " - Use the retrieve_documents tool and fetch the relevant documents from it."
-                 " - After recieving the documents create a response according to the question asked."
-                 " - If none of the documents are retrieved, just tell the user that I cann't provide informaion"
-                 "as no documents are retrieved.")
+UPLOAD_DIR = r"D:\9D Tech Work\Central_repo\Demo\uploads"
